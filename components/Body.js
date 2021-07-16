323503de53b49301bee33319bc8b0d9fab2a8ec9
trusswork <69535736+trusswork@users.noreply.github.com>
2021-07-16 16:11:54 +0300

    weeeh

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,15 @@
 import MusicCard from "./MusicCard"
 
-function Body({data}) {
+function Body({ data }) {
     return (
-        <div className="w-full text-white px-3 h-screen md:flex md:flex-[0.8]">
-            <MusicCard data={data} />
+        <div className="bg-[#0F0F0F] flex w-full text-white px-3 h-screen  md:flex-[0.8] overflow-x-scroll">
+            <div className="flex flex-col">
+                <MusicCard data={data} />
+                <MusicCard data={data} />
+                <MusicCard data={data} />
+                <MusicCard data={data} />
+            </div>
+
         </div>
     )
 }
--- components/MusicCard.js
+++ components/MusicCard.js
@@ -1,20 +1,85 @@
 import Image from 'next/image'
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        marginRight: 10,
+        padding: 10,
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    cover: {
+        width: 250,
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingBottom: theme.spacing(0),
+    },
+    playIcon: {
+        height: 38,
+        width: 38,
+    },
+}));
 
 function MusicCard({ data }) {
+    const classes = useStyles();
+    const theme = useTheme();
+
     return (
 
+        <div style={{ height: "max-content", display: "flex", margin: "20px", overflowx: "scroll", scrollbarwidth: "none" }}>
+            {
+                data.map((item) => (
+                    <Card className={classes.root}>
+                        <CardMedia
+                            className={classes.cover}>
+                            <Image src={item.image} width={250} height={250} objectFit="cover" />
+                        </CardMedia>
 
-        <div className="flex flex-row ">
-            {data.map((item) => (
-                <div key={item.id} className="m-10" >
-                    <Image alt="images" src={item.image} width={250} height={250} objectFit="cover" />
-                    <p className="text-black">{item.name}</p>
-                </div>
+                        <div className={classes.details}>
+                            <CardContent className={classes.content}>
+                                <Typography component="h5" variant="h5">
+                                    Live From Space
+                                </Typography>
+                                <Typography variant="subtitle1" color="textSecondary">
+                                    {item.name}
+                                </Typography>
+                            </CardContent>
+                            <div className={classes.controls}>
+                                <IconButton aria-label="play/pause">
+                                    <PlayArrowIcon className={classes.playIcon} />
+                                </IconButton>
 
-            ))
+                            </div>
+                        </div>
+
+
+                    </Card>
+
+                ))
             }
 
-        </div>
+
+        </div >
+
+
 
 
     )
